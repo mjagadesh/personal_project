80cec782a4d3a04390916cd188c25fbe0d042f04
mjagadesh <jagadesh3m@gmail.com>
2019-12-18 23:35:34 +0530

    login page correction

--- learningpro/learningapp/views.py
+++ learningpro/learningapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def home(request):
 	print("sds")
-	return render(request, 'home.html')
+	return render(request, 'login.html')
 
 def login1(request):
 	print("heelosdaf")
